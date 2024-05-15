0d6b9fef4f0b43a17343a08c32bb3c2e18879a35
Gregoery J. Matthews <gregorymatthews@Gregorys-MacBook-Pro.local>
2024-05-15 17:22:45 -0500

    Bug hotels methods descriptions updated.

--- code/bug_hotels_code.R
+++ code/bug_hotels_code.R
@@ -5,12 +5,11 @@ cong <- read.csv("/Users/gregorymatthews/Dropbox/bug_hotels/data/cong_master.csv
 
 #Remove the DO outliers
 cong <- cong %>% filter(DO_mg_L <= 20)
-
-
 #Model variable selection.  Must be in model: Tot_veg_pct and Water_depth_cm
-#Add Tot_veg_pct and Water_depth_cm and Water_Temp_C and Veg_H (richness and evenness)
+#Add Tot_veg_pct and Water_depth_cm and Water_Temp_C and Veg_H (richness and evenness.  This is just entropy)
 cong <- cong %>% select(Date, Site, Plot, Week, Treatment, Lat, Long,
-                        Inv_abun, Collector, Shredder, Piercer, Scraper, Predator,DO_mg_L )
+                        Inv_abun, Collector, Shredder, Piercer, Scraper, Predator,
+                        DO_mg_L,Tot_veg_pct, Water_depth_cm, Water_Temp_C, Veg_H)
 
 cong <- cong %>% mutate(n_col = Inv_abun*Collector,
                         n_shr = Inv_abun*Shredder,
@@ -51,6 +50,29 @@ table(cong$Week, cong$Plot) #Why is MEAD 4 and 5 not repeated?
 table(cong$Treatment)
 summary(cong$Inv_abun)
 summary(cong$Collector)
+summary(cong$DO_mg_L)
+summary(cong$Tot_veg_pct)
+summary(cong$Water_depth_cm)
+summary(cong$Water_Temp_C)
+
+hist(cong$DO_mg_L)
+hist(cong$Tot_veg_pct)
+hist(cong$Water_depth_cm)
+hist(cong$Water_Temp_C)
+
+#Exploratory Data Viz
+#Tot Veg Pct and water depth remove these from the model
+#Include Veg H
+ggplot(aes(x = DO_mg_L, y = Water_depth_cm, color = as.factor(Treatment)), data = cong) + geom_point()
+ggplot(aes( y = Water_depth_cm, x = as.factor(Treatment)), data = cong) + geom_boxplot()
+ggplot(aes( y = Water_Temp_C, x = as.factor(Treatment)), data = cong) + geom_boxplot()
+ggplot(aes( y = Tot_veg_pct, x = as.factor(Treatment)), data = cong) + geom_boxplot()
+ggplot(aes( y = DO_mg_L, x = as.factor(Treatment)), data = cong) + geom_boxplot()
+ggplot(aes( y = Veg_H, x = as.factor(Treatment)), data = cong) + geom_boxplot()
+
+
+ggplot(aes( y = Collector, x = DO_mg_L), data = cong) + geom_point() + geom_smooth()
+ggplot(aes( y = Collector, x = Water_depth_cm), data = cong) + geom_point() + geom_smooth()
 
 cong %>% group_by(Treatment) %>% summarize()
 
@@ -65,18 +87,23 @@ cong  <- cong %>% mutate(I_meadow = ifelse(Treatment == "meadow",1,0),
                          I_tyhyd = ifelse(Treatment == "tyhyd",1,0),
                          I_typha = ifelse(Treatment == "typha",1,0),
                          I_week = ifelse(Week == "W1",0,1))
+
 Y <- cong %>% select(n_col, n_shr, n_pie, n_scr, n_pre) 
 Y <- as.matrix(Y)
 Y <- matrix(as.integer(Y),nrow = nrow(Y), ncol = ncol(Y))
 #Creating the design matrix
 #Meadow is baseline
+
 ################################################################################
 #Submerged is a better choice for baseline!!!!
 ################################################################################
-X <- cong %>% select(I_submerged, I_tyhyd, I_typha,DO_mg_L, I_week)
+X <- cong %>% select(I_meadow, I_tyhyd, I_typha, DO_mg_L, Water_Temp_C,Veg_H, I_week)
 #X <- cong %>% select(I_meadow,I_submerged, I_tyhyd, I_typha,DO_mg_L)
 X <- data.frame(int = 1,as.matrix(X))
 
+pairs(X)
+
+#Random effects desing matrix
 Z <- matrix(NA, nrow = nrow(cong), ncol = length(unique(cong$Plot)))
 for (i in 1:nrow(cong)) {
   for (j in 1:length(unique(cong$Plot))) {
@@ -84,6 +111,12 @@ for (i in 1:nrow(cong)) {
   }
 }  
 n <- apply(Y, 1, sum)
+
+#Fit a multinomial regression in frequentist world
+#Figure out the correct baseline.  
+library(nnet)
+fit_basic <- nnet::multinom(cbind(n_pre,n_col,n_shr,n_pie,n_scr) ~ I_meadow + I_tyhyd + I_typha + DO_mg_L + Water_Temp_C + Veg_H + I_week, data = cong)
+summary(fit_basic)
 
 library(rjags)
 ####################################
@@ -102,47 +135,7 @@ data <- list(
 )
 
 #With predictors
-model_string <- "
-model{
-
-for (i in 1:N){
-  Y[i,] ~ dmulti(p[i,], n[i])
-
-for (j in 1:J){
-  p[i,j] <- theta[i,j]/sum(theta[i,])
-  }
-
-  for (j in 1:J){
-  theta[i,j] <- exp(inprod(X[i,], beta[,j]) + inprod(Z[i,], b[,j]))
-  }
-  
-  
-}
-  
-  
-for (d in 1:Dx) {
-  for (j in 1:J){
-    beta[d, j] ~ dnorm(0, 0.001) 
-  }
-}
-
-for (d in 1:Dz) {
-  for (j in 1:J){
-    b[d, j] ~ dnorm(0, tau2b) 
-  }
-}
-
-tau2b <- 1/sigma2b
-sigma2b ~  dnorm(0, 0.001)T(0,10000)
-  
-  
-
-
-}"
-
-
-model_string <- "
-model{
+model_string <- "model{
 
 for (i in 1:N){
   Y[i,] ~ dmulti(p[i,], n[i])
@@ -175,15 +168,13 @@ for (d in 1:Dz) {
 
 tau2b <- 1/sigma2b
 sigma2b ~  dnorm(0, 0.001)T(0,10000)
- 
- 
 
 
 }"
 
 #Look at all the paired comparisons. 
-# Look at computing evenness score for each draw and then getting a distribution for evenness.  
-#compute Shannon-Weaver index which is just entropy.  
+#Look at computing evenness score for each draw and then getting a distribution for evenness.  
+#compute Shannon-Weaver index which is just entropy.  sum(pi log pi)
 
 
 setwd("/Users/gregorymatthews/Dropbox/CDSC/bug_hotels/")
@@ -198,11 +189,13 @@ jags <- jags.model("/Users/gregorymatthews/Dropbox/CDSC/bug_hotels/bug_hotels.bu
 
 update(jags, 1000)
 
-ztest <- jags.samples(jags,c('beta','p','b',"sigma2b"),1000, thin = 1)
+# ztest <- jags.samples(jags,c('beta','p','b',"sigma2b"),1000, thin = 1)
+# str(ztest)
+
 z <- jags.samples(jags,c('beta','p','b',"sigma2b"),200000, thin = 200)
 
-save(z, file = "/Users/gregorymatthews/Dropbox/bug_hotels/z.RData")
-
+save(z, file = "/Users/gregorymatthews/Dropbox/bug_hotels/z_20240417.RData")
+load("/Users/gregorymatthews/Dropbox/bug_hotels/z_20240417.RData")
 
 
 
@@ -231,31 +224,121 @@ acf(z$sigma2b[1,,1], lag.max = 100)
 #And then posterior density plots for each of the categories.
 #Using the average level of the random effect.  
 str(z$beta)
+#z$beta is (9x4)
+#X is (62x9)
+#baseline response level is predator
+#baseline treatment is meadow (This needs to change though I think.)
+
+
+#Plot the posteriors of beta
+#DO_mg_L
+dat <- rbind(data.frame(variable = "DO_mg_L",
+                  category = rep(c("p_col", "p_shr", "p_pie", "p_scr"),each= 1000),
+  beta = c(z$beta[5,1,1:1000,1],
+             z$beta[5,2,1:1000,1],
+             z$beta[5,3,1:1000,1],
+             z$beta[5,4,1:1000,1])),
+
+data.frame(variable = "Tot_veg_pct",
+           category = rep(c("p_col", "p_shr", "p_pie", "p_scr"),each= 1000),
+           beta = c(z$beta[6,1,1:1000,1],
+                    z$beta[6,2,1:1000,1],
+                    z$beta[6,3,1:1000,1],
+                    z$beta[6,4,1:1000,1])),
+
+data.frame(variable = "Water_depth_cm",
+           category = rep(c("p_col", "p_shr", "p_pie", "p_scr"),each= 1000),
+           beta = c(z$beta[7,1,1:1000,1],
+                    z$beta[7,2,1:1000,1],
+                    z$beta[7,3,1:1000,1],
+                    z$beta[7,4,1:1000,1])),
+
+data.frame(variable = "Water_temp_C",
+           category = rep(c("p_col", "p_shr", "p_pie", "p_scr"),each= 1000),
+           beta = c(z$beta[8,1,1:1000,1],
+                    z$beta[8,2,1:1000,1],
+                    z$beta[8,3,1:1000,1],
+                    z$beta[8,4,1:1000,1])))
+ggplot(aes(x = beta, color = factor(category)), data = dat) + geom_density() + facet_grid(variable~1) + geom_vline(xintercept = 0)
+
 
 #Find p vectors with average levels 
 newX <- X[1:4,]
-newX$DO_mg_L <- mean(X$DO_mg_L)
+#Set to 2.5
+#newX$DO_mg_L <- mean(X$DO_mg_L)
+newX$DO_mg_L <- 2.5
+#Set to 50
+newX$Tot_veg_pct <- 50
+#newX$Tot_veg_pct <- mean(X$Tot_veg_pct)
+#Set to 100
+#newX$Water_depth_cm <- mean(X$Water_depth_cm)
+newX$Water_depth_cm <- 100
+#Set to 20
+#newX$Water_Temp_C <- mean(X$Water_Temp_C)
+newX$Water_Temp_C <- 20
 diag(newX[2:4,2:4]) <- 1
 
 
-
-#4 treatments, 5 categories 
+#4 treatments, 5 categories
+#Posterior density estimates of 
 results1 <- results2 <- results3 <- list()
 for (i in 1:1000){print(i)
-results1[[i]] <- data.frame(category = rep(c("p_col", "p_shr", "p_pie", "p_scr", "p_pre"),4), treatment = rep(c("meadow","submerged","typhd","typha"),each = 5), p = c(apply(exp(as.matrix(newX) %*% z$beta[,,i,1]),1,function(x){x/sum(x)})))
-results2[[i]] <- data.frame(category = rep(c("p_col", "p_shr", "p_pie", "p_scr", "p_pre"),4), treatment = rep(c("meadow","submerged","typhd","typha"),each = 5), p = c(apply(exp(as.matrix(newX) %*% z$beta[,,i,2]),1,function(x){x/sum(x)})))
-results3[[i]] <- data.frame(category = rep(c("p_col", "p_shr", "p_pie", "p_scr", "p_pre"),4), treatment = rep(c("meadow","submerged","typhd","typha"),each = 5), p = c(apply(exp(as.matrix(newX) %*% z$beta[,,i,3]),1,function(x){x/sum(x)})))
+  results1[[i]] <- data.frame(category = rep(c("p_col", "p_shr", "p_pie", "p_scr", "p_pre"),4), treatment = rep(c("meadow","submerged","typhd","typha"),each = 5), p = c(apply(cbind(exp(as.matrix(newX) %*% z$beta[,,i,1]),1),1,function(x){x/sum(x)})))
+  results2[[i]] <- data.frame(category = rep(c("p_col", "p_shr", "p_pie", "p_scr", "p_pre"),4), treatment = rep(c("meadow","submerged","typhd","typha"),each = 5), p = c(apply(cbind(exp(as.matrix(newX) %*% z$beta[,,i,2]),1),1,function(x){x/sum(x)})))
+  results3[[i]] <- data.frame(category = rep(c("p_col", "p_shr", "p_pie", "p_scr", "p_pre"),4), treatment = rep(c("meadow","submerged","typhd","typha"),each = 5), p = c(apply(cbind(exp(as.matrix(newX) %*% z$beta[,,i,3]),1),1,function(x){x/sum(x)})))
 }
 
-z$beta[5,1:5,1:1000,1][,1:3]
 
 results <- rbind(do.call(rbind,results1),do.call(rbind,results2),do.call(rbind,results3))
-#ggplot(aes(x = p, fill = category), data = results) + geom_histogram() + facet_grid(treatment ~ 1)
 ggplot(aes(x = p, y = after_stat(scaled), color = category), data = results) + geom_density() + facet_grid(treatment ~ 1)
 
-#ggplot(aes(x = p, color = category), data = results) + geom_boxplot() + facet_grid(treatment ~ 1)
-#ggplot(aes(x = p), data = results) + geom_density() + facet_grid(treatment ~ category)
+#Posterior density estimates of betas relative to meadow.
+nsim <- 1000
+dat <- rbind(data.frame(Treatment = rep("submerged",3*nsim),
+                  category = rep(c("p_col", "p_shr", "p_pie", "p_scr"),each = 3*nsim),
+                  beta = c(z$beta[2,1,1:1000,1],z$beta[2,1,1:1000,2],z$beta[2,1,1:1000,3],
+                           z$beta[2,2,1:1000,1],z$beta[2,2,1:1000,2],z$beta[2,2,1:1000,3],
+                           z$beta[2,3,1:1000,1],z$beta[2,3,1:1000,2],z$beta[2,3,1:1000,3],
+                           z$beta[2,4,1:1000,1],z$beta[2,4,1:1000,2],z$beta[2,4,1:1000,3])), 
+             
+             data.frame(Treatment = rep("typhd",3*nsim),
+                        category = rep(c("p_col", "p_shr", "p_pie", "p_scr"),each = 3*nsim),
+                        beta = c(z$beta[3,1,1:1000,1],z$beta[3,1,1:1000,2],z$beta[3,1,1:1000,3],
+                                 z$beta[3,2,1:1000,1],z$beta[3,2,1:1000,2],z$beta[3,2,1:1000,3],
+                                 z$beta[3,3,1:1000,1],z$beta[3,3,1:1000,2],z$beta[3,3,1:1000,3],
+                                 z$beta[3,4,1:1000,1],z$beta[3,4,1:1000,2],z$beta[3,4,1:1000,3])),
+             
+             data.frame(Treatment = rep("typha",3*nsim),
+                        category = rep(c("p_col", "p_shr", "p_pie", "p_scr"),each = 3*nsim),
+                        beta = c(z$beta[4,1,1:1000,1],z$beta[4,1,1:1000,2],z$beta[4,1,1:1000,3],
+                                 z$beta[4,2,1:1000,1],z$beta[4,2,1:1000,2],z$beta[4,2,1:1000,3],
+                                 z$beta[4,3,1:1000,1],z$beta[4,3,1:1000,2],z$beta[4,3,1:1000,3],
+                                 z$beta[4,4,1:1000,1],z$beta[4,4,1:1000,2],z$beta[4,4,1:1000,3])))
 
+ggplot(aes(x = beta, color = category), data = dat) + geom_density() + facet_grid(Treatment ~ 1)
+
+
+
+
+
+
+
+
+hist(c(z$beta[5,1,1:1000,1],z$beta[5,1,1:1000,2],z$beta[5,1,1:1000,3]))
+hist(c(z$beta[5,2,1:1000,1],z$beta[5,2,1:1000,2],z$beta[5,2,1:1000,3]))
+hist(c(z$beta[5,3,1:1000,1],z$beta[5,3,1:1000,2],z$beta[5,3,1:1000,3]))
+hist(c(z$beta[5,4,1:1000,1],z$beta[5,4,1:1000,2],z$beta[5,4,1:1000,3]))
+
+#Tot_veg_pct
+hist(c(z$beta[6,1,1:1000,1],z$beta[6,1,1:1000,2],z$beta[6,1,1:1000,3]))
+hist(c(z$beta[6,2,1:1000,1],z$beta[6,2,1:1000,2],z$beta[6,2,1:1000,3]))
+hist(c(z$beta[6,3,1:1000,1],z$beta[6,3,1:1000,2],z$beta[6,3,1:1000,3]))
+hist(c(z$beta[6,4,1:1000,1],z$beta[6,4,1:1000,2],z$beta[6,4,1:1000,3]))
+
+hist(c(z$beta[6,1,1:1000,1],z$beta[6,1,1:1000,2],z$beta[6,1,1:1000,3]))
+hist(c(z$beta[6,2,1:1000,1],z$beta[6,2,1:1000,2],z$beta[6,2,1:1000,3]))
+hist(c(z$beta[6,3,1:1000,1],z$beta[6,3,1:1000,2],z$beta[6,3,1:1000,3]))
+hist(c(z$beta[6,4,1:1000,1],z$beta[6,4,1:1000,2],z$beta[6,4,1:1000,3]))
 
 ########################################################
 #SCRAP
@@ -464,4 +547,42 @@ library(ggplot2)
 ggplot(aes(x = factor(cat), y = p, color = factor(trt)), data = results) + geom_boxplot()
 
 
+#Old model formulation
 
+model_string <- "model{
+  
+  for (i in 1:N){
+    Y[i,] ~ dmulti(p[i,], n[i])
+    
+    for (j in 1:J){
+      p[i,j] <- theta[i,j]/sum(theta[i,])
+    }
+    
+    for (j in 1:J){
+      theta[i,j] <- exp(inprod(X[i,], beta[,j]) + inprod(Z[i,], b[,j]))
+    }
+    
+    
+  }
+  
+  
+  for (d in 1:Dx) {
+    for (j in 1:J){
+      beta[d, j] ~ dnorm(0, 0.001) 
+    }
+  }
+  
+  for (d in 1:Dz) {
+    for (j in 1:J){
+      b[d, j] ~ dnorm(0, tau2b) 
+    }
+  }
+  
+  tau2b <- 1/sigma2b
+  sigma2b ~  dnorm(0, 0.001)T(0,10000)
+  
+  
+  
+  
+}"
+ 
